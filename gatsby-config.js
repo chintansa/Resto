3e6c057498f8c3c5a0c5455d3bc006616dea537b
Chintan S A <sheshagiriaddoni.c@northeastern.edu>
2023-09-07 00:32:16 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,27 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  siteMetadata:{
+    title: "Simple Recipes",
+    description: " A recipe website built for learning graphql",
+    author: "@whoami",
+    person:{name: " john", age: "32"},
+    simpleData:['item1', 'item2'],
+    complexData : [
+      {name: " john", age: "32"},
+      {name: "susan", age: "21"},
+    ]
+  },
   plugins: [`gatsby-plugin-styled-components`,
             `gatsby-plugin-image`,
             `gatsby-plugin-sharp`,
             `gatsby-transformer-sharp`,
-],
+            {
+              resolve: `gatsby-source-filesystem`,
+              options:{
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+              },
+            },
+]
 }
